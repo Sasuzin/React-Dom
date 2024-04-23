f2dcb20abb710bf0be08eb583f2729dd9516993f
Sasuzin <lfelipepassos1@gmail.com>
2024-04-23 06:56:48 -0300

    começo

---
+++ organo/src/componentes/Banner/index.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner () {
+
+    return (
+        <header className="banner">
+            <img src="/Imagens/banner.png" alt="O banner principal da tela do organo"></img>
+        </header>
+        
+    )
+}
+
+export default Banner
